76e514638ed256fbdb438087b93439a0f1960aeb
SBiruly <131655940+SBiruly@users.noreply.github.com>
2023-09-17 13:58:53 +0530

    Update1 streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("Starting with Streamlit")
-streamlit.header("Practice for Batch2")
-streamlit.text("something to do")
-streamlit.text("but not conform what to do")
-streamlit.text("so writing something blah blah...")
+streamlit.header('Practice for Batch2')
+streamlit.text('something to do')
+streamlit.text('but not conform what to do')
+streamlit.text('so writing something blah blah...')
