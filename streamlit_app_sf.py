c1674732a0d9706e1b123dab8dc9eb7f39e66932
SBiruly <131655940+SBiruly@users.noreply.github.com>
2023-09-20 18:01:17 +0530

    checking

--- streamlit_app_sf.py
+++ streamlit_app_sf.py
@@ -31,3 +31,4 @@ streamlit.text(fruity_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.text(fruityvice_normalized)
+#-----------------------------------------------------------------------------
