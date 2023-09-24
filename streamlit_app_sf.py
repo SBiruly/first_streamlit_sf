73da46c662bab17861b09be57b73343860d4ae13
SBiruly <131655940+SBiruly@users.noreply.github.com>
2023-09-24 23:53:51 +0530

    Update streamlit_app_sf.py

--- streamlit_app_sf.py
+++ streamlit_app_sf.py
@@ -63,6 +63,8 @@ streamlit.header("What fruit would you like to add")
 # To Display fruityvice api response
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
+# -----------------------------------------------------------------------------
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")   
 
 
 
